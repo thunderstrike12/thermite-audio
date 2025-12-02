9322758e914803a00d53b4fe6e031dedc928d0f7
George Bolba <93228212+Gikster007@users.noreply.github.com>
2025-12-02 11:31:36 +0100

    fix: removed unused batch script (#110)
    Co-authored-by: MΛX <mxcop.dev@gmail.com>

--- engine/assets/shaders/compile.cmd
+++
@@ -1,7 +0,0 @@
-slangc test.slang -profile vs_6_0 -target spirv -o test-vert.spv -entry vertex_entry
-slangc test.slang -profile ps_6_0 -target spirv -o test-frag.spv -entry fragment_entry
-
-slangc debug-line.slang -profile vs_6_0 -target spirv -o debug-line-vert.spv -entry vertex_entry
-slangc debug-line.slang -profile ps_6_0 -target spirv -o debug-line-frag.spv -entry fragment_entry
-
-pause
