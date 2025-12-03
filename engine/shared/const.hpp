b55028e2ecbc34a1ba7d5dfaa6407f0d75c003d2
MΛX <44031065+mxcop@users.noreply.github.com>
2025-12-03 14:37:19 +0100

    fix: clang tidy (#124)

--- engine/shared/const.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-/* Large 32-bit floating point constant. */
-constexpr float BIG_F32 = 1e30f;
-
-constexpr int VOXELS_PER_UNIT = 10;
-constexpr float UNITS_PER_VOXEL = 1.0f / VOXELS_PER_UNIT;
-constexpr float VOXEL_SIZE_HALF = 0.5f / VOXELS_PER_UNIT;
-constexpr float VOXEL_SIZE_SQR = UNITS_PER_VOXEL * UNITS_PER_VOXEL;
--- engine/shared/pch.hpp
+++ engine/shared/pch.hpp
@@ -7,4 +7,14 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp> /* vector math */
 
-#include "const.hpp" /* constants */
+/* Large 32-bit floating point constant. */
+constexpr float BIG_F32 = 1e30f;
+
+/* Number of voxels per unit in world-space. */
+constexpr uint32_t VOXELS_PER_UNIT = 10u;
+/* Number of world-space units per voxel. */
+constexpr float UNITS_PER_VOXEL = 1.0f / VOXELS_PER_UNIT;
+/* Half the size of a voxel in world-space units. */
+constexpr float VOXEL_SIZE_HALF = 0.5f / VOXELS_PER_UNIT;
+/* Size of a voxel in world-space units squared. */
+constexpr float VOXEL_SIZE_SQR = UNITS_PER_VOXEL * UNITS_PER_VOXEL;
