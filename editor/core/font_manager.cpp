d05e23a6477664d2d6105d2227435f26ce7c0162
Jaeden <71218810+ThatOneLazyGuy@users.noreply.github.com>
2026-01-29 15:15:30 +0100

    fix: icon size (#361)

--- editor/core/font_manager.cpp
+++ editor/core/font_manager.cpp
@@ -12,8 +12,8 @@ void tmt::FontManager::init() {
     const uint16_t glyph_ranges[] = {ICON_MIN_MS, ICON_MAX_MS, 0};
     ImFontConfig config {};
     config.MergeMode = true;
-    config.GlyphOffset.y = 9.f;
-    load("MaterialSymbols", {IO::Location::EDITOR, "fonts/MaterialSymbolsRounded.ttf"}, 37.f, config, glyph_ranges);
+    config.GlyphOffset.y = 7.f;
+    load("MaterialSymbols", {IO::Location::EDITOR, "fonts/MaterialSymbolsRounded.ttf"}, 32.f, config, glyph_ranges);
 }
 
 void tmt::FontManager::load(const std::string& name, const IO::FileLocation& location, const float size, const ImFontConfig& config, const uint16_t* glyph_ranges) {
---
+++ editor/imgui/types/audio.cpp
@@ -0,0 +1,45 @@
+#include "audio.hpp"
+
+#include "engine/core/audio.hpp"
+#include "engine/tools/serializer.hpp"
+#include "engine/tools/serializer/audio.hpp"
+
+void tag_invoke(ImReflect::ImInput_t, const char* label, tmt::AudioEvent& value, ImSettings& settings, ImResponse& response) {
+    if (ImGui::Button(ICON_MS_CLEAR)) value = {};
+    ImGui::SameLine();
+
+    ImGui::BeginDisabled();
+    std::string path = (value.is_valid() ? value.get_path() : "");
+    ImReflect::Input(label, path, settings, response);
+    ImGui::EndDisabled();
+
+    if (!ImGui::BeginDragDropTarget()) return;
+
+    const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("AudioEvent");
+    if (payload != nullptr) {
+        const std::string_view json_string {static_cast<char*>(payload->Data), static_cast<size_t>(payload->DataSize)};
+        tmt::Serializer::deserialize(nlohmann::ordered_json::parse(json_string), value);
+    }
+
+    ImGui::EndDragDropTarget();
+}
+
+void tag_invoke(ImReflect::ImInput_t, const char* label, tmt::VolumeControl& value, ImSettings& settings, ImResponse& response) {
+    if (ImGui::Button(ICON_MS_CLEAR)) value = {};
+    ImGui::SameLine();
+
+    ImGui::BeginDisabled();
+    std::string path = (value.is_valid() ? value.get_path() : "");
+    ImReflect::Input(label, path, settings, response);
+    ImGui::EndDisabled();
+
+    if (!ImGui::BeginDragDropTarget()) return;
+
+    const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("VolumeControl");
+    if (payload != nullptr) {
+        const std::string_view json_string {static_cast<char*>(payload->Data), static_cast<size_t>(payload->DataSize)};
+        tmt::Serializer::deserialize(nlohmann::ordered_json::parse(json_string), value);
+    }
+
+    ImGui::EndDragDropTarget();
+}
