0a84d43df701d07b1d814e595f12362e70207328
Bogdan Mocanu <bogdan.game.development@gmail.com>
2026-02-10 17:09:37 +0100

    refactor: basic actions for the game (#532)

---
+++ projects/game/components/game_input.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace game {
+namespace action {
+constexpr auto MOVE_FORWARD = "move_forward";
+constexpr auto MOVE_BACKWARD = "move_backward";
+constexpr auto MOVE_LEFT = "move_left";
+constexpr auto MOVE_RIGHT = "move_right";
+constexpr auto MOVE_UP = "move_up";
+constexpr auto MOVE_DOWN = "move_down";
+constexpr auto BREAK = "break";
+constexpr auto SHOOT = "shoot";
+constexpr auto SWITCH_WEAPON_1 = "switch_1";
+constexpr auto SWITCH_WEAPON_2 = "switch_2";
+constexpr auto SWITCH_WEAPON_3 = "switch_3";
+}  // namespace action
+}  // namespace game
--- projects/game/components/player.cpp
+++ projects/game/components/player.cpp
@@ -7,19 +7,61 @@
 #include "engine/core/components/camera.hpp"
 
 #include "engine/core/window.hpp"
+#include "engine/core/input/input_map.hpp"
+#include "game_input.hpp"
+// TODO before we have a serializer for input, you can add all the needed keybindings here.
+//  TODO we still have to add the gamepad inputs here
+
+using namespace game;
+void setup_inputs(tmt::InputMap& input_map) {
+    // movement
+    input_map.add_action(action::MOVE_FORWARD);
+    input_map.add_key_to_action(action::MOVE_FORWARD, tmt::Key::W);
+
+    input_map.add_action(action::MOVE_BACKWARD);
+    input_map.add_key_to_action(action::MOVE_BACKWARD, tmt::Key::S);
+
+    input_map.add_action(action::MOVE_LEFT);
+    input_map.add_key_to_action(action::MOVE_LEFT, tmt::Key::A);
+
+    input_map.add_action(action::MOVE_RIGHT);
+    input_map.add_key_to_action(action::MOVE_RIGHT, tmt::Key::D);
+
+    input_map.add_action(action::MOVE_UP);
+    input_map.add_key_to_action(action::MOVE_UP, tmt::Key::SPACE);
+
+    input_map.add_action(action::MOVE_DOWN);
+    input_map.add_key_to_action(action::MOVE_DOWN, tmt::Key::LEFT_SHIFT);
+
+    input_map.add_action(action::BREAK);
+    input_map.add_key_to_action(action::BREAK, tmt::Key::LEFT_CTRL);
+
+    // combat
+    input_map.add_action(action::SHOOT);
+    input_map.add_action_mouse(action::SHOOT, tmt::MouseButton::LEFT);
+
+    input_map.add_action(action::SWITCH_WEAPON_1);
+    input_map.add_key_to_action(action::SWITCH_WEAPON_1, tmt::Key::NUM_1);
+    input_map.add_action(action::SWITCH_WEAPON_2);
+    input_map.add_key_to_action(action::SWITCH_WEAPON_2, tmt::Key::NUM_2);
+    input_map.add_action(action::SWITCH_WEAPON_3);
+    input_map.add_key_to_action(action::SWITCH_WEAPON_3, tmt::Key::NUM_3);
+}
 
 void Player::start() {
     tmt::engine.input.set_mouse_relative_to_window(true);
     glm::vec2 screen_size = {tmt::engine.window.width, tmt::engine.window.height};
     tmt::engine.input.warp_mouse({screen_size.x / 2.0f, screen_size.y / 2.0f}, true);
     tmt::engine.input.lock_mouse(true);
+    setup_inputs(tmt::engine.input_map);
 }
 
 void Player::update(const tmt::FrameData& time) {
     auto& transform = tmt::engine.ecs.get_component<tmt::Transform>(entity);
     auto& camera = tmt::engine.ecs.get_component<tmt::Camera>(entity);
-    const float dx = tmt::engine.input.get_mouse_delta_x();
-    const float dy = tmt::engine.input.get_mouse_delta_y();
+    auto& input = tmt::engine.input;
+    const float dx = input.get_mouse_delta_x();
+    const float dy = input.get_mouse_delta_y();
 
     // Mouse Look (unchanged)
     camera.yaw -= dx * camera_sensitivity;
@@ -35,16 +77,15 @@ void Player::update(const tmt::FrameData& time) {
 
     // Calculate desired movement direction (unchanged from demo)
     glm::vec3 input_dir = {0.0f, 0.0f, 0.0f};
-    if (tmt::engine.input.is_keyboard_button_pressed(tmt::Key::W)) input_dir += transform.get_forward();
-    if (tmt::engine.input.is_keyboard_button_pressed(tmt::Key::S)) input_dir -= transform.get_forward();
-    if (tmt::engine.input.is_keyboard_button_pressed(tmt::Key::D)) input_dir += transform.get_right();
-    if (tmt::engine.input.is_keyboard_button_pressed(tmt::Key::A)) input_dir -= transform.get_right();
-    if (tmt::engine.input.is_keyboard_button_pressed(tmt::Key::SPACE)) input_dir += glm::vec3(0.0f, 1.0f, 0.0f);
-    if (tmt::engine.input.is_keyboard_button_pressed(tmt::Key::LEFT_SHIFT)) input_dir -= glm::vec3(0.0f, 1.0f, 0.0f);
+    if (input.is_action_pressed(action::MOVE_FORWARD)) input_dir += transform.get_forward();
+    if (input.is_action_pressed(action::MOVE_BACKWARD)) input_dir -= transform.get_forward();
+    if (input.is_action_pressed(action::MOVE_RIGHT)) input_dir += transform.get_right();
+    if (input.is_action_pressed(action::MOVE_LEFT)) input_dir -= transform.get_right();
+    if (input.is_action_pressed(action::MOVE_UP)) input_dir += glm::vec3(0.0f, 1.0f, 0.0f);
+    if (input.is_action_pressed(action::MOVE_DOWN)) input_dir -= glm::vec3(0.0f, 1.0f, 0.0f);
 
     // Added button for breaking, port from prototype
-    bool breaking = false;
-    if (tmt::engine.input.is_keyboard_button_pressed(tmt::Key::LEFT_CTRL)) breaking = true;
+    bool breaking = input.is_action_pressed(action::BREAK);
 
     // Apply acceleration or drag
     if (glm::length(input_dir) > 0.0f) {
