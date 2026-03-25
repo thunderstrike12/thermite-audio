74da04810e3d8f501e94d128f873d277e8e775f0
Jaeden <71218810+ThatOneLazyGuy@users.noreply.github.com>
2026-03-25 14:00:19 +0100

    feat: colorwheel widget popup (#911)

--- editor/shared/imgui_color_wheel.hpp
+++ editor/shared/imgui_color_wheel.hpp
@@ -74,7 +74,6 @@ inline bool ColorWheel3(const char* label, float color[3], ImGuiColorWheelFlags 
     ImGuiWindow* window = GetCurrentWindow();
     if (window->SkipItems) return false;
 
-    ImDrawList* draw_list = window->DrawList;
     const ImGuiStyle& style = g.Style;
     const ImGuiIO& io = g.IO;
 
@@ -86,23 +85,10 @@ inline bool ColorWheel3(const char* label, float color[3], ImGuiColorWheelFlags 
     const bool set_current_color_edit_id = (g.ColorEditCurrentID == 0);
     if (set_current_color_edit_id) g.ColorEditCurrentID = window->IDStack.back();
 
-    BeginGroup();
-
     // Setup.
     const int style_alpha8 = IM_F32_TO_INT8_SAT(style.Alpha);
     const ImU32 black = IM_COL32(0, 0, 0, style_alpha8);
     const ImU32 white = IM_COL32(255, 255, 255, style_alpha8);
-
-    const ImVec2 picker_pos = window->DC.CursorPos;
-
-    const float bars_width = GetFrameHeight();                                                                      // Arbitrary smallish width of Hue/Alpha picking bars.
-    const float hs_picker_size = ImMax(bars_width * 1.0f, width - 1.0f * (bars_width + style.ItemInnerSpacing.x));  // Hue/Saturation/ picking circle.
-    const float bar_pos_x = picker_pos.x + hs_picker_size + style.ItemInnerSpacing.x;
-    const float handle_half_height = bars_width * 0.25f;
-
-    const float wheel_thickness = hs_picker_size * 0.08f;
-    const float wheel_radius = hs_picker_size * 0.5f;
-    const ImVec2 wheel_center(picker_pos.x + hs_picker_size * 0.5f, picker_pos.y + hs_picker_size * 0.5f);
 
     bool hs_cursor_active = false;
     bool v_cursor_active = false;
@@ -119,93 +105,122 @@ inline bool ColorWheel3(const char* label, float color[3], ImGuiColorWheelFlags 
     saturation = ImClamp(saturation, 0.0f, 1.0f);
     value = ImClamp(value, 0.0f, 1.0f);
 
-    // Handle hue & saturation circle user input.
-    InvisibleButton("HueSaturation", ImVec2(hs_picker_size, hs_picker_size));
-    if (IsItemActive() && !is_readonly) {
-        // Mouse position inside the circle as normalized device coordinates [-1,1]
-        const ImVec2 circle_ndc = ImClamp((io.MousePos - wheel_center) / wheel_radius, ImVec2(-1.0f, -1.0f), ImVec2(1.0f, 1.0f));
-        // Calculate the saturation of the clicked point on the circle.
-        saturation = ImMin(1.0f, ImLength(circle_ndc));
-        // Calculate the hue of the clicked point on the circle.
-        hue = ImFmod(ImAtan2(-circle_ndc.y, -circle_ndc.x) / IM_TAU + 0.5f + 0.75f, 1.0f);
-        hs_cursor_active = true;
+    const float frame_height = GetFrameHeight();
+    const ImU32 square_color = ColorConvertFloat4ToU32(ImVec4(srgb_color[0], srgb_color[1], srgb_color[2], 1.0f));
+    const ImVec2 size(frame_height, frame_height);
+    window->DrawList->AddRectFilled(window->DC.CursorPos, window->DC.CursorPos + size, square_color);
+    if (InvisibleButton(label, size) && !IsPopupOpen("WheelPopup")) OpenPopup("WheelPopup");
+    SameLine();
+    Text("%s", label);
+
+    BeginGroup();
+
+    bool changed = false;
+    if (BeginPopup("WheelPopup", ImGuiWindowFlags_NoMove)) {
+        const ImGuiWindow* popup_window = GetCurrentWindow();
+        const ImVec2 picker_pos = popup_window->DC.CursorPos;
+
+        const float bars_width = GetFrameHeight();                                                                      // Arbitrary smallish width of Hue/Alpha picking bars.
+        const float hs_picker_size = ImMax(bars_width * 1.0f, width - 1.0f * (bars_width + style.ItemInnerSpacing.x));  // Hue/Saturation/ picking circle.
+        const float bar_pos_x = picker_pos.x + hs_picker_size + style.ItemInnerSpacing.x;
+        const float handle_half_height = bars_width * 0.25f;
+
+        const float wheel_thickness = hs_picker_size * 0.08f;
+        const float wheel_radius = hs_picker_size * 0.5f;
+        const ImVec2 wheel_center(picker_pos.x + hs_picker_size * 0.5f, picker_pos.y + hs_picker_size * 0.5f);
+
+        // Handle hue & saturation circle user input.
+        InvisibleButton("HueSaturation", ImVec2(hs_picker_size, hs_picker_size));
+        if (IsItemActive() && !is_readonly) {
+            // Mouse position inside the circle as normalized device coordinates [-1,1]
+            const ImVec2 circle_ndc = ImClamp((io.MousePos - wheel_center) / wheel_radius, ImVec2(-1.0f, -1.0f), ImVec2(1.0f, 1.0f));
+            // Calculate the saturation of the clicked point on the circle.
+            saturation = ImMin(1.0f, ImLength(circle_ndc));
+            // Calculate the hue of the clicked point on the circle.
+            hue = ImFmod(ImAtan2(-circle_ndc.y, -circle_ndc.x) / IM_TAU + 0.5f + 0.75f, 1.0f);
+            hs_cursor_active = true;
+        }
+
+        // Handle value bar user input.
+        SetCursorScreenPos(ImVec2(bar_pos_x, picker_pos.y));
+        InvisibleButton("Value", ImVec2(bars_width, hs_picker_size));
+        if (IsItemActive() && !is_readonly) {
+            // Calculate the value of the value bar of the clicked point on the bar.
+            value = 1.0f - glm::clamp((io.MousePos.y - (picker_pos.y + handle_half_height)) / (hs_picker_size - (handle_half_height * 2.0f) - 1.0f), 0.0f, 1.0f);
+            v_cursor_active = true;
+        }
+
+        // Only update the color with the new HSV values if the values changed.
+        changed = hs_cursor_active || v_cursor_active;
+        if (changed) {
+            // Update the input color.
+            ColorConvertHSVtoRGB(hue, saturation, value, srgb_color[0], srgb_color[1], srgb_color[2]);
+
+            std::memcpy(color, srgb_color, sizeof(srgb_color));
+
+            // If the value changed, and we are using a custom color space, we have to convert the rec709 back to the custom colorspace.
+            if (flags & ImGuiColorWheelFlags_CustomColorSpace) ConvertColorSpace(CUSTOM_COLOR_MATRIX, color);
+        }
+
+        // If we do want the manual input colors we add the drag float after converting back, this ensures that the colorspace is always right.
+        if (!(flags & ImGuiColorWheelFlags_NoInputs)) changed |= DragFloat3("RGB", color, 0.1f, 0.0f, 1.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp | ImGuiSliderFlags_ColorMarkers);
+
+        ImDrawList* draw_list = popup_window->DrawList;
+
+        // Draw the color wheel in segments.
+        const int wheel_segment_count = draw_list->_CalcCircleAutoSegmentCount(wheel_radius);
+        for (int i = 0; i < wheel_segment_count; i++) {
+            const float segment1 = static_cast<float>(i) / static_cast<float>(wheel_segment_count - 1);
+            const float x1 = ImCos(IM_TAU * segment1) * wheel_radius;
+            const float y1 = ImSin(IM_TAU * segment1) * wheel_radius;
+
+            const float segment2 = static_cast<float>((i + 1) % wheel_segment_count) / static_cast<float>(wheel_segment_count - 1);
+            const float x2 = ImCos(IM_TAU * segment2) * wheel_radius;
+            const float y2 = ImSin(IM_TAU * segment2) * wheel_radius;
+
+            ImVec4 rgb1(0.0f, 0.0f, 0.0f, 1.0f);
+            ColorConvertHSVtoRGB(ImFmod(segment1 + 0.75f, 1.0f), 1.0f, value, rgb1.x, rgb1.y, rgb1.z);
+            const ImU32 color1 = ColorConvertFloat4ToU32(rgb1);
+
+            ImVec4 rgb2(0.0f, 0.0f, 0.0f, 1.0f);
+            ColorConvertHSVtoRGB(ImFmod(segment2 + 0.75f, 1.0f), 1.0f, value, rgb2.x, rgb2.y, rgb2.z);
+            const ImU32 color2 = ColorConvertFloat4ToU32(rgb2);
+
+            const ImU32 no_saturation_color = ColorConvertFloat4ToU32(ImVec4(value, value, value, 1.0f));
+
+            const ImVec2 uv_white = GetFontTexUvWhitePixel();
+            draw_list->PrimReserve(3, 3);
+            draw_list->PrimVtx(wheel_center + ImVec2(x2, y2), uv_white, color2);
+            draw_list->PrimVtx(wheel_center, uv_white, no_saturation_color);
+            draw_list->PrimVtx(wheel_center + ImVec2(x1, y1), uv_white, color1);
+        }
+
+        // Calculate the hue & saturation wheel cursor position.
+        const float hs_cursor_angle = hue * IM_TAU + (IM_PI * 0.5f);
+        const float hs_cursor_dist = saturation * wheel_radius;
+        const ImVec2 hs_cursor_pos(wheel_center.x + ImCos(hs_cursor_angle) * hs_cursor_dist, wheel_center.y + ImSin(hs_cursor_angle) * hs_cursor_dist);
+
+        // Draw hue & saturation wheel cursor/preview circle.
+        const float hs_cursor_radius = hs_cursor_active ? wheel_thickness * 0.55f : wheel_thickness * 0.40f;
+        const int hs_cursor_segments = draw_list->_CalcCircleAutoSegmentCount(hs_cursor_radius);  // Lock segment count so the +1 one matches others.
+        draw_list->AddCircleFilled(hs_cursor_pos, hs_cursor_radius, ColorConvertFloat4ToU32(ImVec4(srgb_color[0], srgb_color[1], srgb_color[2], 1.0f)), hs_cursor_segments);
+        draw_list->AddCircle(hs_cursor_pos, hs_cursor_radius + 1, black, hs_cursor_segments);
+        draw_list->AddCircle(hs_cursor_pos, hs_cursor_radius, white, hs_cursor_segments);
+
+        // Draw value bar.
+        draw_list->AddRectFilledMultiColor(ImVec2(bar_pos_x, picker_pos.y), ImVec2(bar_pos_x + bars_width, picker_pos.y + hs_picker_size), white, white, black, black);
+
+        // Draw value bar handle.
+        const float value_selection_y = picker_pos.y + handle_half_height + (hs_picker_size - (handle_half_height * 2.0f) - 1.0f) * (1 - value);
+        const ImVec2 selection_min(bar_pos_x, value_selection_y - handle_half_height);
+        const ImVec2 selection_max(bar_pos_x + bars_width, value_selection_y + handle_half_height);
+        draw_list->AddRectFilled(selection_min, selection_max, white);
+        draw_list->AddRect(selection_min, selection_max, black);
+
+        EndPopup();
     }
-
-    // Handle value bar user input.
-    SetCursorScreenPos(ImVec2(bar_pos_x, picker_pos.y));
-    InvisibleButton("Value", ImVec2(bars_width, hs_picker_size));
-    if (IsItemActive() && !is_readonly) {
-        // Calculate the value of the value bar of the clicked point on the bar.
-        value = 1.0f - glm::clamp((io.MousePos.y - (picker_pos.y + handle_half_height)) / (hs_picker_size - (handle_half_height * 2.0f) - 1.0f), 0.0f, 1.0f);
-        v_cursor_active = true;
-    }
-
-    // Only update the color with the new HSV values if the values changed.
-    bool changed = hs_cursor_active || v_cursor_active;
-    if (changed) {
-        // Update the input color.
-        ColorConvertHSVtoRGB(hue, saturation, value, srgb_color[0], srgb_color[1], srgb_color[2]);
-
-        std::memcpy(color, srgb_color, sizeof(srgb_color));
-
-        // If the value changed, and we are using a custom color space, we have to convert the rec709 back to the custom colorspace.
-        if (flags & ImGuiColorWheelFlags_CustomColorSpace) ConvertColorSpace(CUSTOM_COLOR_MATRIX, color);
-    }
-
-    // If we do want the manual input colors we add the drag float after converting back, this ensures that the colorspace is always right.
-    if (!(flags & ImGuiColorWheelFlags_NoInputs)) changed |= DragFloat3("RGB", color, 0.1f, 0.0f, 1.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp | ImGuiSliderFlags_ColorMarkers);
 
     EndGroup();
-
-    // Draw the color wheel in segments.
-    const int wheel_segment_count = draw_list->_CalcCircleAutoSegmentCount(wheel_radius);
-    for (int i = 0; i < wheel_segment_count; i++) {
-        const float segment1 = static_cast<float>(i) / static_cast<float>(wheel_segment_count - 1);
-        const float x1 = ImCos(IM_TAU * segment1) * wheel_radius;
-        const float y1 = ImSin(IM_TAU * segment1) * wheel_radius;
-
-        const float segment2 = static_cast<float>((i + 1) % wheel_segment_count) / static_cast<float>(wheel_segment_count - 1);
-        const float x2 = ImCos(IM_TAU * segment2) * wheel_radius;
-        const float y2 = ImSin(IM_TAU * segment2) * wheel_radius;
-
-        ImVec4 rgb1(0.0f, 0.0f, 0.0f, 1.0f);
-        ColorConvertHSVtoRGB(ImFmod(segment1 + 0.75f, 1.0f), 1.0f, value, rgb1.x, rgb1.y, rgb1.z);
-        const ImU32 color1 = ColorConvertFloat4ToU32(rgb1);
-
-        ImVec4 rgb2(0.0f, 0.0f, 0.0f, 1.0f);
-        ColorConvertHSVtoRGB(ImFmod(segment2 + 0.75f, 1.0f), 1.0f, value, rgb2.x, rgb2.y, rgb2.z);
-        const ImU32 color2 = ColorConvertFloat4ToU32(rgb2);
-
-        const ImU32 no_saturation_color = ColorConvertFloat4ToU32(ImVec4(value, value, value, 1.0f));
-
-        const ImVec2 uv_white = GetFontTexUvWhitePixel();
-        draw_list->PrimReserve(3, 3);
-        draw_list->PrimVtx(wheel_center + ImVec2(x2, y2), uv_white, color2);
-        draw_list->PrimVtx(wheel_center, uv_white, no_saturation_color);
-        draw_list->PrimVtx(wheel_center + ImVec2(x1, y1), uv_white, color1);
-    }
-
-    // Calculate the hue & saturation wheel cursor position.
-    const float hs_cursor_angle = hue * IM_TAU + (IM_PI * 0.5f);
-    const float hs_cursor_dist = saturation * wheel_radius;
-    const ImVec2 hs_cursor_pos(wheel_center.x + ImCos(hs_cursor_angle) * hs_cursor_dist, wheel_center.y + ImSin(hs_cursor_angle) * hs_cursor_dist);
-
-    // Draw hue & saturation wheel cursor/preview circle.
-    const float hs_cursor_radius = hs_cursor_active ? wheel_thickness * 0.55f : wheel_thickness * 0.40f;
-    const int hs_cursor_segments = draw_list->_CalcCircleAutoSegmentCount(hs_cursor_radius);  // Lock segment count so the +1 one matches others.
-    draw_list->AddCircleFilled(hs_cursor_pos, hs_cursor_radius, ColorConvertFloat4ToU32(ImVec4(srgb_color[0], srgb_color[1], srgb_color[2], 1.0f)), hs_cursor_segments);
-    draw_list->AddCircle(hs_cursor_pos, hs_cursor_radius + 1, black, hs_cursor_segments);
-    draw_list->AddCircle(hs_cursor_pos, hs_cursor_radius, white, hs_cursor_segments);
-
-    // Draw value bar.
-    draw_list->AddRectFilledMultiColor(ImVec2(bar_pos_x, picker_pos.y), ImVec2(bar_pos_x + bars_width, picker_pos.y + hs_picker_size), white, white, black, black);
-
-    // Draw value bar handle.
-    const float value_selection_y = picker_pos.y + handle_half_height + (hs_picker_size - (handle_half_height * 2.0f) - 1.0f) * (1 - value);
-    const ImVec2 selection_min(bar_pos_x, value_selection_y - handle_half_height);
-    const ImVec2 selection_max(bar_pos_x + bars_width, value_selection_y + handle_half_height);
-    draw_list->AddRectFilled(selection_min, selection_max, white);
-    draw_list->AddRect(selection_min, selection_max, black);
 
     if (changed && g.LastItemData.ID != 0)  // In case of ID collision, the second EndGroup() won't catch g.ActiveId
         MarkItemEdited(g.LastItemData.ID);
