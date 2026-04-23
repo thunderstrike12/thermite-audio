537a0670e1a41d49f4818420807e7e19a7b831a5
MikaHalb <45827473+Yungsaas@users.noreply.github.com>
2026-04-23 13:49:44 +0200

    feat: respawning prefabs on spawner component (#1040)

--- projects/game/components/world_gen/random_prefab_spawner.cpp
+++ projects/game/components/world_gen/random_prefab_spawner.cpp
@@ -5,25 +5,48 @@
 #include "engine/tools/prefab_helper.hpp"
 
 void game::RandomPrefabSpawner::start() {
+    respawn_timer = respawn_time;
+    spawn_random_prefab();
+}
+
+void game::RandomPrefabSpawner::update(const tmt::FrameData& time) {
+    if (!tmt::engine.ecs.valid(spawned_entity)) {
+        // spawned entity is invalid -> has been destroyed
+        // set to entt::null to ensure that no other entity with the same ID makes this valid again
+        spawned_entity = entt::null;
+    }
+    // spawned entity has either been destroyed or was not spawned in (can happen when prefabs get set during runtime)
+    if (spawned_entity == entt::null) {
+        respawn_timer += time.delta_time;
+        if (respawn_timer >= respawn_time) {
+            if (spawn_count < spawn_limit || spawn_limit == 0) {
+                if (re_randomize) {
+                    spawn_random_prefab();
+                } else {
+                    spawn_prefab(last_spawned_prefab);
+                }
+            }
+        }
+    }
+}
+
+void game::RandomPrefabSpawner::end() {
+}
+
+void game::RandomPrefabSpawner::spawn_random_prefab() {
     if (prefabs.empty()) {
+        tmt::Log::error("No prefabs found on prefab spawner component, entity: {}", entity);
         return;
     }
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, static_cast<int>(prefabs.size()) - 1);
 
-    int random_index = dis(gen);
+    int random_index = dis(rng);
 
     // Spawn the randomly selected prefab
     spawn_prefab(prefabs[random_index]);
 }
 
-void game::RandomPrefabSpawner::update(const tmt::FrameData& time) {}
-
-void game::RandomPrefabSpawner::end() {}
-
-void game::RandomPrefabSpawner::spawn_prefab(const tmt::ResourceRef<tmt::Json>& prefab) const {
+void game::RandomPrefabSpawner::spawn_prefab(const tmt::ResourceRef<tmt::Json>& prefab) {
     auto instantiated = tmt::PrefabHelper::instantiate_prefab(prefab->file_location);
     auto& transform = tmt::engine.ecs.get_component<tmt::Transform>(instantiated);
     auto& transform_parent = tmt::engine.ecs.get_component<tmt::Transform>(entity);
@@ -33,5 +56,8 @@ void game::RandomPrefabSpawner::spawn_prefab(const tmt::ResourceRef<tmt::Json>& 
     }
     transform.set_world_position(transform_parent.get_world_position());
 
-    // transform.set_world_rotation(transform_parent.get_world_rotation());
+    spawned_entity = instantiated;
+    last_spawned_prefab = prefab;
+    respawn_timer = 0.0f;
+    spawn_count++;
 }
--- projects/game/components/world_gen/random_prefab_spawner.hpp
+++ projects/game/components/world_gen/random_prefab_spawner.hpp
@@ -2,9 +2,7 @@
 #include "engine/systems/gameplay/game_component.hpp"
 
 namespace game {
-
-struct RandomPrefabSpawner : public tmt::GameComponent<RandomPrefabSpawner> {
-   public:
+struct RandomPrefabSpawner : tmt::GameComponent<RandomPrefabSpawner> {
     using GameComponent::GameComponent;
 
     static std::string_view get_name() { return "Random Prefab Spawner"; }
@@ -16,9 +14,18 @@ struct RandomPrefabSpawner : public tmt::GameComponent<RandomPrefabSpawner> {
 
     std::vector<tmt::ResourceRef<tmt::Json>> prefabs;
 
-   private:
-    void spawn_prefab(const tmt::ResourceRef<tmt::Json>& prefab) const;
-};
+    int spawn_limit = 1;
+    float respawn_time = 20.0f;
+    bool re_randomize = true;
 
-}  // namespace game
-TMT_OBJECT(game::RandomPrefabSpawner, (prefabs));
+private:
+    void spawn_random_prefab();
+    void spawn_prefab(const tmt::ResourceRef<tmt::Json>& prefab);
+    tmt::Entity spawned_entity = entt::null;
+    tmt::ResourceRef<tmt::Json> last_spawned_prefab;
+    std::mt19937 rng{ std::random_device{}() };
+    int spawn_count = 0;
+    float respawn_timer = 20.0f;
+};
+} // namespace game
+TMT_OBJECT(game::RandomPrefabSpawner, (prefabs, spawn_limit, respawn_time, re_randomize));
