feb018cb5f6586089f986c5408f70bf04757f619
Bogdan Mocanu <bogdan.game.development@gmail.com>
2026-05-05 12:06:06 +0200

    fix: destroy component uses delta (#1056)

--- projects/game/components/gameplay_functionality_components/destroy_timed.cpp
+++ projects/game/components/gameplay_functionality_components/destroy_timed.cpp
@@ -1,6 +1,6 @@
 #include "destroy_timed.hpp"
 void game::DestroyTimed::update(const tmt::FrameData& time) {
-    time_to_destroy -= tmt::engine.frame_data().elapsed_time;
+    time_to_destroy -= tmt::engine.frame_data().delta_time;
     if (time_to_destroy < 0.0f) {
         tmt::engine.ecs.destroy_entity(entity);
     }
