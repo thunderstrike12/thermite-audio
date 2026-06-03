86045303af0ec1422a7bbeee034eaf225216bcfe
Bogdan Mocanu <bogdan.game.development@gmail.com>
2026-06-03 11:08:09 +0200

    fix: audio slider persistent (#1266)

--- projects/game/components/game_settings/audio_volume_change.cpp
+++ projects/game/components/game_settings/audio_volume_change.cpp
@@ -1,15 +1,23 @@
 #include "audio_volume_change.hpp"
+
+#include "engine/tools/player_data.hpp"
 void game::AudioVolumeChangeComponent::start() {
     auto* slider { tmt::engine.ecs.try_get_component<tmt::Slider>(entity) };
     if (slider == nullptr) {
         return;
     }
-    slider->value = volume_control.get_volume();
+    auto name { tmt::engine.ecs.get_component<tmt::Name>(entity).name };
+    auto& saved_value { tmt::engine.player_data.get<float>("Slider_" + name, volume_control.get_volume()) };
+    volume_control.set_volume(saved_value);
+    slider->value = saved_value;
     slider->on_value_changed.add(this, &AudioVolumeChangeComponent::on_value_changed);
 }
 void game::AudioVolumeChangeComponent::on_value_changed(tmt::Slider::Context context) {
     if (context.disabled) return;
     volume_control.set_volume(context.value);
+    auto name { tmt::engine.ecs.get_component<tmt::Name>(entity).name };
+    auto& saved_value { tmt::engine.player_data.get<float>("Slider_" + name, volume_control.get_volume()) };
+    saved_value = context.value;
 }
 void game::AudioVolumeChangeComponent::update(const tmt::FrameData& time) {}
 void game::AudioVolumeChangeComponent::end() {
